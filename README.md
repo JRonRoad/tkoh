a997fc7da8bf833dbd7f8595f614cecdfadcd0b1
JierunCHEN <jrchan@zju.edu.cn>
2021-03-26 17:23:44 +0800

    For api demo

---
+++ README.md
@@ -0,0 +1,11 @@
+## Requirements
+
+Python 3.8 or later with all `requirements.txt` dependencies installed, including `torch>=1.7`. To install run:
+```bash
+$ pip install -r requirements.txt
+```
+
+## Running Pipeline
+
+Loading model > loading img > inference > results processing.
+Please refer to the `main.py` file. Note that current model is just for api demo. It needs more training later to ensure the performance.
---
+++ main.py
@@ -0,0 +1,30 @@
+import torch
+
+# Load Model
+model = torch.hub.load('ultralytics/yolov5', 'custom',  path_or_model='model_20210326.pt')
+
+# Model attributes, currently set as:
+model.conf = 0.08  # confidence threshold (0-1)
+model.iou = 0.3  # NMS IoU threshold (0-1)
+
+# Load Image
+img_path = 'input/4.jpeg'
+# a variety of input format supported:
+#   filename:   img = 'data/zidane.jpg'
+#   URI:             = 'https://github.com/ultralytics/yolov5/releases/download/v1.0/zidane.jpg'
+#   OpenCV:          = cv2.imread('image.jpg')[:,:,::-1]  # HWC BGR to RGB x(720,1280,3)
+#   PIL:             = Image.open('image.jpg')  # HWC x(720,1280,3)
+#   numpy:           = np.zeros((720,1280,3))  # HWC
+#   torch:           = torch.zeros(16,3,720,1280)  # BCHW
+#   multiple:        = [Image.open('image1.jpg'), Image.open('image2.jpg'), ...]  # list of images
+
+# Inference
+results = model(img_path, size=640)
+
+# Post processing
+results.print()  # or .show(), .save()
+results.save()  # save processed images to directory /results/
+
+items_detected = results.pred[0].shape[0]
+guide_wire = True if items_detected>0 else False
+print("Guide wire exists!" if guide_wire else "No wire!")
